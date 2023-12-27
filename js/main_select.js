0c080e8ecfb1a6ab86a7ceee765eab2ddb066443
SMGXGAMER842 <141907473+SMGXSCRIPTS@users.noreply.github.com>
2023-12-27 03:56:00 -0800

    Create main_select.js

---
+++ js/main_select.js
@@ -0,0 +1,9 @@
+function redirect_linux()
+{
+    window.location.href = "docs/linux/zemuth-linux.html";
+}
+
+function redirect_progl()
+{
+    window.location.href = "docs/progl/zemuth-progl.html";
+}
